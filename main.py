39f9df7b0cd9a99d93f6613ced9584fb9759c188
Taylor <taylor.smith2@uky.edu>
2018-04-27 10:59:49 -0400

    Add evaluation and visualization

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten
 from keras.callbacks import ModelCheckpoint
 from keras.utils import to_categorical as cat
 from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_curve, average_precision_score
+import matplotlib.pyplot as plt
+import os
 
 # ------------------------------- PARAMETERS ------------------------------------- #
-data_path = 'data/w_zeroes/'
-base_fname = 'ssx'
+# Data params
+dataset = 'w_zeroes'    # Dataset selection
+modelName = 'test'      # Directory name for saved model information
+
+base_fname = 'ssx'      # filenames ssx#.txt
 n_ex = 3815
 
-n_epochs = 20
-n_splits = 10
-batch_size = 32
+# Training params
+n_epochs = 5           # Number of epochs to train each split
+n_splits = 8           # Number of splits for cross-validation
+batch_size = 32         # Training batch size
 # -------------------------------------------------------------------------------- #
+
+data_path = 'data/' + dataset + '/'
+model_path = 'models/' + dataset + '/' + modelName + '/'
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
 
 # Load inputs
 data = []
@@ -24,7 +36,7 @@ data = np.array(data)
 data = np.expand_dims(data, -1)
 
 # Load labels and make categorical (i.e. if 2 labels, each label is of form (0, 1) or (1, 0)
-labels = np.loadtxt(data_path+'labels.txt', dtype=int)
+labels = np.loadtxt(data_path+'labels.txt', dtype=int)[:n_ex]
 split_labels = labels
 labels = cat(labels, 2)
 
@@ -43,30 +55,62 @@ def get_model(in_shape):
     flat = Flatten()(pool_2)
     x = Dense(2, activation='softmax', name='predictions')(flat)
     my_model = Model(input=inputs, output=x)
-    my_model.compile(loss='binary_crossentropy', optimizer='SGD')
+    my_model.compile(loss='binary_crossentropy', optimizer='SGD', )
     return my_model
 
 
 # Stratify data into separate folds for cross-validation
 skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=1)
 
+model = get_model(data.shape[1:])
+# Open the file
+with open(model_path + 'model_summary.txt','w') as fh:
+    # Pass the file handle in as a lambda function to make it callable
+    model.summary(print_fn=lambda x: fh.write(x + '\n'))
 
+f = open(model_path + 'evaluation.txt', 'w')
+f.write('Fold_Num,Precision,Recall,F1\n')
 for i, (train_idx, val_idx) in enumerate(skf.split(data, split_labels)):
     print('Evaluating on Fold ' + str(i+1) + ' of ' + str(n_splits) + '.')
-    mcp_save = ModelCheckpoint(data_path+'CNN_Model_fold' + str(i+1) + '.h5',
-                               save_best_only=True,monitor='val_loss', mode='min')
+    mcp_save = ModelCheckpoint(model_path+'trained_model_fold' + str(i+1) + '.h5',
+                               save_best_only=True, monitor='val_loss', mode='min')
 
     X_train = data[train_idx]
     y_train = labels[train_idx]
     X_val = data[val_idx]
     y_val = labels[val_idx]
+    y_eval = split_labels[val_idx]
 
     model = get_model(data.shape[1:])
     history = model.fit(X_train, y_train, batch_size=batch_size, epochs=n_epochs,
-              callbacks=[mcp_save, ], validation_data=(X_val, y_val))
+                        callbacks=[mcp_save, ], validation_data=(X_val, y_val))
     loss = history.history['loss']
     loss = np.array(loss)
-    lossFilename = data_path+'CNN_Model_fold' + str(i+1) + '_loss.txt'
-    np.savetxt(lossFilename, loss, fmt='%.4f')
-    print(model.evaluate(X_val, y_val))
+    plt.figure()
+    plt.plot(np.arange(len(loss))+1, loss)
+    plt.ylabel('Validation Loss')
+    plt.xlabel('Epoch Number')
+    plt.title('Validation Loss vs. Epoch\nFold ' + str(i+1))
+    plt.savefig(model_path + 'fold' + str(i+1) + '_loss.png')
+    y_pred = model.predict(X_val)
+    y_pred = y_pred[:, 0] < 0.5
+    prec = precision_score(y_eval, y_pred)
+    rec = recall_score(y_eval, y_pred)
+    f1 = f1_score(y_eval, y_pred)
+    f.write('%d,%.4f,%.4f,%.4f\n' % (i+1, prec, rec, f1))
+
+    pcurve, rcurve, _ = precision_recall_curve(y_eval, y_pred)
+    plt.step(rcurve, pcurve, color='b', alpha=0.2,
+             where='post')
+    plt.fill_between(rcurve, pcurve, where=None, alpha=0.2,
+                     color='b')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Fold ' + str(i+1) + ' Precision-Recall curve: AP=%0.2f' % average_precision_score(y_eval, y_pred))
+    plt.savefig(model_path + 'fold' + str(i+1) + '_PRcurve.png')
+    plt.close()
+
+f.close()
 
